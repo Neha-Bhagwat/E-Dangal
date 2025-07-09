238e61e999c2f3ae533f75c870cdc96d23c6fa4d
Aarati Dhamele <f20230641@hyderabad.bits-pilani.ac.in>
2025-07-10 01:20:32 +0530

    Update main.dart

--- UI Team/lib/main.dart
+++ UI Team/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'The Flutter Way - Template',
+      title: 'E-Dangal;',
       theme: AppTheme.lightTheme(context),
       initialRoute: SplashScreen.routeName,
       routes: routes,
